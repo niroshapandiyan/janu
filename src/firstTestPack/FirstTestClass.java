3ae93e33dc04959fd610311170f863bbd4efae52
niroshapandiyan <niroshapandiyan8@gmail.com>
2023-03-19 11:22:32 +0530

    Second Commit

---
+++ src/firstTestPack/FirstTestClass.java
@@ -0,0 +1,11 @@
+package firstTestPack;
+
+public class FirstTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Eclipse");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author niros
+ *
+ */
+module FirstTestProj {
+}
